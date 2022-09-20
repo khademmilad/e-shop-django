5428035654c64916ae0d1b4dd876bb302cc27957
khademmilad <khademmilad@gmail.com>
2022-09-20 22:16:00 +0200

    added static files and wrote first view

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home
+
+
+urlpatterns = [
+    path('', home, name="home"),
+
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Text from view</h1>')
